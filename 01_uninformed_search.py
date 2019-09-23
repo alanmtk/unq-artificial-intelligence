eb51478d0761aa397361e9bcda869dc97d8ca64e
Alan Matkorski <alanmatkorski@gmail.com>
2019-09-23 00:22:12 -0300

    uninformed search refactor

--- 01_uninformed_search.py
+++ 01_uninformed_search.py
@@ -1,47 +1,48 @@
 from collections import deque
 
-PROBLEM_GRAPH = {
-    ('👴🥬🐑🐺', ''): {
-        '->️👴🐑': ('🥬🐺', '👴🐑'),
-    },
-    ('👴🥬🐑', '🐺'): {
-        '->️👴🐑': ('🥬', '👴🐑🐺'),
-        '->️👴🥬': ('🐑', '👴🥬🐺'),
-    },
-    ('👴🥬🐺', '🐑'): {
-        '->️👴🥬': ('🐺', '👴🥬🐑'),
-        '->️👴🐺': ('🥬', '👴🐑🐺'),
-        '->️👴': ('🥬🐺', '👴🐑'),
-    },
-    ('👴🐑🐺', '🥬'): {
-        '->️👴🐑': ('🐺', '👴🥬🐑'),
-        '->️👴🐺': ('🐑', '👴🥬🐺'),
-    },
-    ('🥬🐺', '👴🐑'): {
-        '<-️👴': ('👴🥬🐺', '🐑'),
-        '<-️👴🐑': ('👴🥬🐑🐺', ''),
-    },
-    ('👴🐑', '🥬🐺'): {
-        '->️👴🐑': ('', '👴🥬🐑🐺'),
-        '->️👴': ('🐑', '👴🥬🐺'),
-    },
-    ('🥬', '👴🐑🐺'): {
-        '️<-👴🐑': ('👴🥬🐑', '🐺'),
-        '️<-👴🐺': ('👴🥬🐺', '🐑'),
-    },
-    ('🐑', '👴🥬🐺'): {
-        '️<-👴🥬': ('👴🥬🐑', '🐺'),
-        '️<-👴🐺': ('👴🐑🐺', '🥬'),
-        '️<-👴': ('👴🐑', '🥬🐺'),
-    },
-    ('🐺', '👴🥬🐑'): {
-        '️<-👴🐑': ('👴🐑🐺', '🥬'),
-        '️<-👴🥬': ('👴🥬🐺', '🐑'),
-    },
-    ('', '👴🥬🐑🐺'): {
-        '<-️👴🐑': ('👴🐑', '🥬🐺'),
-    },
-}
+
+def sort(state):
+    return ''.join(list(filter(lambda token: token in state, list('👴🥬🐑🐺'))))
+
+
+def is_valid_move(side):
+    return not ('🥬🐑' in side or '🐑🐺' in side)
+
+
+def expand(state):
+    moves = dict()
+    _from = 0 if '👴' in state[0] else 1
+    _to = 1 if '👴' in state[0] else 0
+    _arrow = '->' if _from == 0 else '<-'
+    _from_elements = list(state[_from])
+
+    for a in _from_elements:
+        moving = ''.join({'👴', a})
+        key = _arrow + moving
+        _new_origin = sort(''.join(list(filter(lambda e: e not in moving, _from_elements))))
+        _new_destination = sort(state[_to] + moving)
+        if is_valid_move(_new_origin):
+            if _from == 0:
+                moves[key] = (_new_origin, _new_destination)
+            else:
+                moves[key] = (_new_destination, _new_origin)
+    return moves
+
+
+def build_graph():
+    graph = dict()
+    queue = deque()
+    queue.append(('👴🥬🐑🐺', ''))
+
+    while queue:
+        node = queue.popleft()
+        if node not in graph:
+            graph[node] = expand(node)
+            neighbors = list(graph[node].values())
+            for neighbor in neighbors:
+                if neighbor not in graph:
+                    queue.append(neighbor)
+    return graph
 
 
 def dfs(graph, initial, is_goal):
@@ -51,8 +52,9 @@ def dfs(graph, initial, is_goal):
     visited.add(initial)
     while stack:
         node, path = stack.pop()
+        print(node, path)
         if is_goal(node):
-            return node, path
+            return path, len(path), len(visited)
         node_edges = graph[node]
         for edge_label, target_node in node_edges.items():
             if target_node not in visited:
@@ -68,8 +70,9 @@ def bfs(graph, initial, is_goal):
     visited.add(initial)
     while queue:
         node, path = queue.popleft()
+        print(node, path)
         if is_goal(node):
-            return node, path
+            return path, len(path), len(visited)
         node_edges = graph[node]
         for edge_label, target_node in node_edges.items():
             if target_node not in visited:
@@ -82,6 +85,10 @@ def is_goal(node):
     return node == ('', '👴🥬🐑🐺')
 
 
-print(dfs(PROBLEM_GRAPH, ('👴🥬🐑🐺', ''), is_goal))
+# knowledge representation
+graph = build_graph()
 
-print(bfs(PROBLEM_GRAPH, ('👴🥬🐑🐺', ''), is_goal))
+print('DFS')
+print(dfs(graph, ('👴🥬🐑🐺', ''), is_goal))
+print('BFS')
+print(bfs(graph, ('👴🥬🐑🐺', ''), is_goal))
